1bc53e855aca029e9e7aa9e2f07a5e58565ab77e
yblmmen <161982180+yblmmen@users.noreply.github.com>
2024-03-23 13:06:45 +0900

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -2,20 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const accountRoutes = require('./routes/accountRoutes');
-
 const app = express();
-const port = 3000;
 
+// Express 애플리케이션 설정
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('public')); // 'public' 디렉토리의 정적 파일 제공
 app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: true,
+    secret: 'secret', // 세션 쿠키를 서명하는데 사용되는 비밀키
+    resave: false, // 세션을 항상 저장할 지 여부 (변경되지 않아도 저장)
+    saveUninitialized: true, // 초기화되지 않은 세션을 저장소에 강제로 저장
 }));
 
+// 계정 관련 라우트 설정
 app.use('/account', accountRoutes);
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+// app 객체를 모듈로 내보냄
+module.exports = app;
